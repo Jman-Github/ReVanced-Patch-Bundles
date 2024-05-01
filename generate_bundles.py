5a7a198d5f56a8deabc4bc734adf32e38b0a30ca
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-05-01 06:54:03 -0400

    Update generate_bundles.py

--- generate_bundles.py
+++ generate_bundles.py
@@ -2,12 +2,11 @@ import asyncio
 import json
 from httpx import AsyncClient
 
-async def get_latest_release(repo_url, prerelease = False):
+async def get_latest_release(repo_url, prerelease=False):
     async def get_version_url(release):
         version = release['tag_name']
         for asset in release["assets"]:
-            if asset["browser_download_url"].endswith(".jar")  or\
-               asset["browser_download_url"].endswith(".apk"):
+            if asset["browser_download_url"].endswith(".jar") or asset["browser_download_url"].endswith(".apk"):
                 asset_url = asset['browser_download_url']
                 return version, asset_url
         print(f"No asset found for the {version}")
@@ -17,29 +16,28 @@ async def get_latest_release(repo_url, prerelease = False):
     response = await AsyncClient().get(api_url)
     if response.status_code == 200:
         releases = response.json()
-        if prerelease:
-            for release in releases:
-                if release["prerelease"]:
-                    version, asset_url = await get_version_url(release)
-                    return version, asset_url
-        else:
-            for release in releases:
-                if not release["prerelease"]:
-                    version, asset_url = await get_version_url(release)
-                    return version, asset_url
-    print(f"No release found for the {repo_url}")
-    return None, None
-
+        target_release = None
+        for release in releases:
+            if prerelease == release["prerelease"]:
+                target_release = release
+                break
+        if not target_release:
+            print(f"No {'pre' if prerelease else ''}release found for {repo_url}")
+            return None, None
+        version, asset_url = await get_version_url(target_release)
+        return version, asset_url
 
 async def main():
     with open('bundle-sources.json') as file:
         sources = json.load(file)
 
     for source, repo in sources.items():
-        patches_version, patches_asset_url = await get_latest_release(repo.get('patches'),
-                    repo.get('prerelease', False))
-        integration_version, integration_asset_url = await get_latest_release(repo.get('integration'),
-                    repo.get('prerelease', False))
+        patches_version, patches_asset_url = await get_latest_release(repo.get('patches'), repo.get('prerelease', False))
+        if not patches_version:  # If no pre-release found, try regular release
+            patches_version, patches_asset_url = await get_latest_release(repo.get('patches'))
+        integration_version, integration_asset_url = await get_latest_release(repo.get('integration'), repo.get('prerelease', False))
+        if not integration_version:  # If no pre-release found, try regular release
+            integration_version, integration_asset_url = await get_latest_release(repo.get('integration'))
         info_dict = {
             "patches": {
                 "version": patches_version,
