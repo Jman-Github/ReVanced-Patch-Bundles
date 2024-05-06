9af0b411dd2ae6c0c08d46d1d1dc8701a7e3382e
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-05-05 20:42:14 -0400

    Update find_commit.py

--- find_commit.py
+++ find_commit.py
@@ -2,11 +2,15 @@ import os
 import requests
 
 repo = "Jman-Github/ReVanced-Patch-Bundles"
-author = "github-actions%5Bbot%5D"
+author = "github-actions[bot]"
 
 files = os.popen('git diff --name-only HEAD^ -- "*.json"').read().splitlines()
 
 for file in files:
     url = f"https://api.github.com/repos/{repo}/commits?author={author}&path={file}"
-    commit_sha = requests.get(url).json()[0]['sha']
-    print(f"- [{file}](https://github.com/{repo}/commit/{commit_sha})")
+    response = requests.get(url).json()
+    if response:
+        commit_sha = response[0]['sha']
+        print(f"- [{file}](https://github.com/{repo}/commit/{commit_sha})")
+    else:
+        print(f"No commits found for {file}")
