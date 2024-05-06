06aa901fd5dc825407537105b6f15086c7ce5840
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-05-05 22:32:18 -0400

    Update find_commit.py

--- find_commit.py
+++ find_commit.py
@@ -1,26 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
 
-repo = "Jman-Github/ReVanced-Patch-Bundles"
-author = "github-actions[bot]"
-
-url = f"https://github.com/{repo}/commits?author={author}"
+# URL for the commits page
+url = "https://github.com/Jman-Github/ReVanced-Patch-Bundles/commits?author=github-actions%5Bbot%5D"
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 
-commit_links = soup.find_all('a', class_='link-gray-dark no-underline h4 js-navigation-open')
-latest_commit_link = commit_links[0]['href'] if commit_links else None
-
-if latest_commit_link:
-    commit_url = f"https://github.com{latest_commit_link}"
-    commit_response = requests.get(commit_url)
-    commit_soup = BeautifulSoup(commit_response.text, 'html.parser')
-    files_modified = commit_soup.find_all('div', class_='file-info flex-auto min-width-0')
+# Find the latest commit link
+commit_link = soup.find('a', class_='js-navigation-open link-gray-dark no-underline')
+if commit_link:
+    latest_commit_url = "https://github.com" + commit_link['href']
     
-    print("Changed files:")
-    for file_info in files_modified:
-        file_name = file_info.find('a', class_='link-gray-dark text-bold js-navigation-open').text.strip()
-        file_path = file_info.find('a', class_='link-gray').get('title')
-        print(f"- [{file_name}]({file_path})")
+    # Get the latest commit page
+    latest_commit_response = requests.get(latest_commit_url)
+    latest_commit_soup = BeautifulSoup(latest_commit_response.text, 'html.parser')
+    
+    # Find all the modified files in the latest commit
+    files_modified = latest_commit_soup.find_all('div', class_='file-info flex-auto min-width-0')
+
+    # Print file names and links
+    with open('changed_files.txt', 'w') as f:
+        print("Changed files:")
+        for file_info in files_modified:
+            file_name = file_info.find('a', class_='link-gray-dark text-bold js-navigation-open').text.strip()
+            file_path = file_info.find('a', class_='link-gray').get('title')
+            f.write(f"- [{file_name}]({file_path})\n")
+            print(f"- [{file_name}]({file_path})")
 else:
     print("No commits found.")
