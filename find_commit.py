1eeb035b4d6f27f2453588cb6ddef8bd4b236cdf
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-05-05 22:19:35 -0400

    Update find_commit.py

--- find_commit.py
+++ find_commit.py
@@ -1,21 +1,26 @@
-import os
 import requests
+from bs4 import BeautifulSoup
 
 repo = "Jman-Github/ReVanced-Patch-Bundles"
 author = "github-actions[bot]"
 
-files = os.popen('git diff --name-only HEAD^ -- "*.json"').read().splitlines()
+url = f"https://github.com/{repo}/commits?author={author}"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
 
-for file in files:
-    url = f"https://api.github.com/repos/{repo}/commits?author={author}"
-    response = requests.get(url).json()
-    commit_sha = None
-    for commit in response:
-        files_modified = [f['filename'] for f in commit['files']]
-        if file in files_modified:
-            commit_sha = commit['sha']
-            break
-    if commit_sha:
-        print(f"- [{file}](https://github.com/{repo}/commit/{commit_sha})")
-    else:
-        print(f"No commits found for {file}")
+commit_links = soup.find_all('a', class_='link-gray-dark no-underline h4 js-navigation-open')
+latest_commit_link = commit_links[0]['href'] if commit_links else None
+
+if latest_commit_link:
+    commit_url = f"https://github.com{latest_commit_link}"
+    commit_response = requests.get(commit_url)
+    commit_soup = BeautifulSoup(commit_response.text, 'html.parser')
+    files_modified = commit_soup.find_all('div', class_='file-info flex-auto min-width-0')
+    
+    print("Changed files:")
+    for file_info in files_modified:
+        file_name = file_info.find('a', class_='link-gray-dark text-bold js-navigation-open').text.strip()
+        file_path = file_info.find('a', class_='link-gray').get('title')
+        print(f"- [{file_name}]({file_path})")
+else:
+    print("No commits found.")
