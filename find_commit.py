925673c8994cf30153f94008f2dccd5648bcd92c
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-05-05 22:51:09 -0400

    Update find_commit.py

--- find_commit.py
+++ find_commit.py
@@ -2,32 +2,20 @@ import requests
 from bs4 import BeautifulSoup
 
 # URL for the commits page
-url = "https://github.com/Jman-Github/ReVanced-Patch-Bundles/commits?author=github-actions%5Bbot%5D"
+url = "https://api.github.com/repos/jman-github/revanced-patch-bundles/commits"
 response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
+commits = response.json()
 
-# Find the latest commit link
-commit_link = soup.find('a', class_='js-navigation-open link-gray-dark no-underline')
-if commit_link:
-    latest_commit_url = "https://github.com" + commit_link['href']
-    
-    # Get the latest commit page
-    latest_commit_response = requests.get(latest_commit_url)
-    latest_commit_soup = BeautifulSoup(latest_commit_response.text, 'html.parser')
-    
-    # Find all the modified files in the latest commit
-    files_modified = latest_commit_soup.find_all('div', class_='file-info flex-auto min-width-0')
+# Find the latest commit made by "github-actions[bot]"
+latest_commit_url = None
+for commit in commits:
+    if commit['commit']['author']['name'] == "github-actions[bot]":
+        latest_commit_url = commit['html_url']
+        break
 
-    if files_modified:
-        # Print file names and links
-        with open('changed_files.txt', 'w') as f:
-            print("Changed files:")
-            for file_info in files_modified:
-                file_name = file_info.find('a', class_='link-gray-dark text-bold js-navigation-open').text.strip()
-                file_path = file_info.find('a', class_='link-gray').get('title')
-                f.write(f"- [{file_name}]({file_path})\n")
-                print(f"- [{file_name}]({file_path})")
-    else:
-        print("No files found in the latest commit.")
+if latest_commit_url:
+    with open('changed_files.txt', 'w') as f:
+        f.write(f"Latest commit by github-actions[bot]: [{latest_commit_url}]({latest_commit_url})\n")
+        print(f"Latest commit by github-actions[bot]: [{latest_commit_url}]({latest_commit_url})")
 else:
     print("No commits found.")
