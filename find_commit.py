67063a6e0ddf7503db27363fa42a7796eb3f668d
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-05-09 10:49:04 -0400

    Update find_commit.py

--- find_commit.py
+++ find_commit.py
@@ -5,15 +5,15 @@ url = "https://api.github.com/repos/jman-github/revanced-patch-bundles/commits"
 response = requests.get(url)
 commits = response.json()
 
-# Find the latest commit made by "github-actions[bot]"
+# Find the latest commit URL made by "github-actions[bot]"
 latest_commit_url = None
 for commit in commits:
-    if commit['commit']['author']['name'] == "github-actions[bot]":
+    if commit['author']['login'] == "github-actions[bot]":
         latest_commit_url = commit['html_url']
         break
 
 if latest_commit_url:
-    with open('changed_files.txt', 'w') as f:
-        f.write(f"\n[View Commit]({latest_commit_url})\n")
+    with open('find_commit_output.txt', 'w') as f:
+        f.write(f"[View Commit]({latest_commit_url})\n")
 else:
     print("No commits found.")
