c9ac9e47041591fc2153fa1d989b30fad65ed8dd
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-07-23 15:26:05 -0400

    Update generate_bundles.py

--- generate_bundles.py
+++ generate_bundles.py
@@ -52,10 +52,11 @@ async def get_latest_release(repo_url, prerelease, latest_flag=False):
         
         if target_release:
             version, patches_url, integrations_url = await get_version_urls(target_release)
-            if not patches_url or not integrations_url:
-                print(f"Release {target_release['tag_name']} does not contain required assets (.jar or .apk)")
+            if patches_url and integrations_url:
+                return version, patches_url, integrations_url
+            else:
+                print(f"Release {target_release['tag_name']} does not contain both required assets (.jar and .apk)")
                 return None, None, None
-            return version, patches_url, integrations_url
         else:
             print(f"No {'pre' if prerelease else ''}release found for {repo_url}")
             return None, None, None
@@ -85,8 +86,10 @@ async def fetch_release_data(source, repo):
         print(f"Latest release information saved to {source}-patches-bundle.json")
         
         subprocess.run(["git", "add", f"{source}-patches-bundle.json"])
+        subprocess.run(["git", "commit", "-m", f"Update {source}-patches-bundle.json to latest"])
+        subprocess.run(["git", "push", "origin", "bundles"])
     else:
-        print(f"Error: Unable to fetch release information for {source}. Check if the releases contain .jar and .apk assets.")
+        print(f"Error: Unable to fetch complete release information for {source}. Ensure releases contain both .jar and .apk assets.")
 
 async def main():
     with open('bundle-sources.json') as file:
@@ -98,9 +101,6 @@ async def main():
     for source, repo in sources.items():
         await fetch_release_data(source, repo)
         await asyncio.sleep(0)  # Add a cooldown of (seconds) seconds between requests
-    
-    subprocess.run(["git", "commit", "-m", "Update patch-bundle.json to latest"])
-    subprocess.run(["git", "push", "origin", "bundles"])
 
 if __name__ == "__main__":
     asyncio.run(main())
