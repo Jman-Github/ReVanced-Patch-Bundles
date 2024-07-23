a76b8cf825a736a980578f32ea3926de5a841419
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-07-23 14:10:28 -0400

    Update generate_bundles.py

--- generate_bundles.py
+++ generate_bundles.py
@@ -7,7 +7,7 @@ import os
 
 # Function to get the PAT from an environment variable or a secure location
 def get_github_pat():
-    return os.getenv('GITHUB_PAT')  # Ensure you have set this environment variable
+    return os.getenv('GH_PAT')  # Ensure you have set this environment variable
 
 # Function to print rate limit status
 async def print_rate_limit_status(client):
