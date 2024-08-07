2a614a156e73fb00885f87851985f51c6212cc05
Jman <128645077+Jman-Github@users.noreply.github.com>
2024-08-06 21:16:10 -0400

    feat: Switcehd to Github Actions bot for manager link updates

--- update_readme.py
+++ update_readme.py
@@ -4,7 +4,6 @@ import requests
 import base64
 
 def update_readme(artifact_url):
-    readme_path = os.path.join(os.environ["GITHUB_WORKSPACE"], "README.md")
     headers = {
         "Authorization": f"Bearer {os.environ['GIT_TOKEN']}",
         "Content-Type": "application/json"
@@ -17,12 +16,20 @@ def update_readme(artifact_url):
         readme_content = response.json()
         readme_content_decoded = base64.b64decode(readme_content["content"]).decode("utf-8")
         lines = readme_content_decoded.split("\n")
-        lines[220] = f"{artifact_url}"
+        lines[209] = f"{artifact_url}"
         new_content = "\n".join(lines)
         update_data = {
             "message": "Updated manager download link to latest",
             "content": base64.b64encode(new_content.encode()).decode("utf-8"),
-            "sha": readme_content["sha"]
+            "sha": readme_content["sha"],
+            "committer": {
+                "name": "github-actions[bot]",
+                "email": "41898282+github-actions[bot]@users.noreply.github.com"
+            },
+            "author": {
+                "name": "github-actions[bot]",
+                "email": "41898282+github-actions[bot]@users.noreply.github.com"
+            }
         }
         response = requests.put(
             f"https://api.github.com/repos/{os.environ['GITHUB_REPOSITORY']}/contents/README.md",
